fa91264e189a2d9a8c233a85aa2bc27f9a0ac2bf
Ken Otero <105245483+KennethOtero@users.noreply.github.com>
2022-07-21 12:39:22 -0400

    Update AddTask.js
    file path fix

--- scripts/AddTask.js
+++ scripts/AddTask.js
@@ -42,7 +42,7 @@ function ajax() {
 
         // AJAX
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "../content/Process_AddTask.php");
+        xhr.open("POST", "../content/restricted/Process_AddTask.php");
         xhr.onload = function() {
             var result = xhr.responseText;
             if (result == "Success") {
