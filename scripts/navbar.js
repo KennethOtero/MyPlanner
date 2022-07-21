4d8fb19a485b6a9ef8b81e1a2d4a19c18cb68574
Ken Otero <105245483+KennethOtero@users.noreply.github.com>
2022-07-21 12:37:20 -0400

    Update navbar.js
    menu improvements

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,24 +1,25 @@
 // Get hamburger icon and navigation bar
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.getElementById("nav-menu");
 
-// Hide menu when user clicks outside of it
+// Manage menu click events
 document.addEventListener('mouseup', function(e) {
+    // Open/Close menu on click
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }); 
+
     // Hide menu when clicking outside of it
     if (!navMenu.contains(e.target)) {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }
 
-    // Open menu on click
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    });
+    // Close hamburger menu when an item is clicked
+    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
 });
 
-// Close hamburger menu when an item is clicked
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}));
